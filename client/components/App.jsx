a072aa15e572113243aa4255d7b4046c93840be1
Wonil Park <wonil.park3@outlook.com>
2020-05-05 16:06:35 -0700

    gallery view buttons: added transition effect and functionality

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -15,6 +15,9 @@ class App extends React.Component {
     };
 
     this.handleClick = this.handleClick.bind(this);
+    this.viewClickHandler = this.viewClickHandler.bind(this);
+    this.showAllPhotos = this.showAllPhotos.bind(this);
+    this.toggleLike = this.toggleLike.bind(this);
   }
 
   componentDidMount() {
@@ -38,6 +41,27 @@ class App extends React.Component {
     });
   }
 
+  viewClickHandler(value) {
+    if (value === 'close') {
+      this.setState({ galleryView: false });
+    } else {
+      this.setState({ index: value });
+    }
+  }
+
+  showAllPhotos() {
+    this.setState({
+      galleryView: true,
+      index: 0,
+    });
+  }
+
+  toggleLike() {
+    let value;
+    this.state.liked === 0 ? value = 1 : value = 0;
+    this.setState({ liked: value });
+  }
+
   render() {
     const {
       liked, photos, galleryView, index,
@@ -45,7 +69,7 @@ class App extends React.Component {
     if (photos.length > 0 && !galleryView) {
       return (
         <div className="photo-gallery">
-          <div className="liked">
+          <div className="liked" id="like-btn" onClick={this.toggleLike}>
             {liked ? (
               <div className="liked-container">
                 <i className="fas fa-heart" />
@@ -62,21 +86,23 @@ class App extends React.Component {
             <div className="container">
               <div className="round-corner">
                 <div className="left-half">
-                  <div className="firstcolumn">
+                  <div className="first-column">
                     <Photo className="photo photo-0" index="0" photo={photos[0]} handleClick={this.handleClick} />
                   </div>
                 </div>
                 <div className="right-half">
-                  <div className="secondcolumn">
+                  <div className="second-column">
                     <Photo className="photo photo-1" index="1" photo={photos[1]} handleClick={this.handleClick} />
                     <div className="border" />
                     <Photo className="photo photo-2" index="2" photo={photos[2]} handleClick={this.handleClick} />
                   </div>
-                  <div className="thirdcolumn">
+                  <div className="third-column">
                     <Photo className="photo photo-3" index="3" photo={photos[3]} handleClick={this.handleClick} />
                     <div className="border" />
                     <Photo className="photo photo-4" index="4" photo={photos[4]} handleClick={this.handleClick} />
-                    <button type="submit" className="show-all" onClick={this.handleClick}>Show all photos</button>
+                    <button type="submit" className="show-all" id="all-photos" onClick={this.showAllPhotos}>
+                      Show all photos
+                    </button>
                   </div>
                 </div>
               </div>
@@ -88,7 +114,9 @@ class App extends React.Component {
     if (photos.length > 0 && galleryView) {
       return (
         <div className="gallery-view-container">
-          <Gallery index={index} photos={photos} liked={liked} />
+          <Gallery index={index} photos={photos} liked={liked} viewClickHandler={this.viewClickHandler} toggleLike={this.toggleLike}>
+
+          </Gallery>
         </div>
       );
     }
